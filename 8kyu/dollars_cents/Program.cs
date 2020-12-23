1b30dea0d2bcf9afc6606d0ddb9f6a6c6c2d8a7b
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2020-12-23 12:47:20 +0200

    Solution for codewars&dollars_cents tasks added

---
+++ 8kyu/dollars_cents/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+/*
+Code wars
+Level: 8kyu
+Description:
+    The company you work for has just been awarded a contract to build a payment gateway. 
+    In order to help move things along, you have volunteered to create a function that will 
+    take a float and return the amount formatting in dollars and cents.
+*/
+namespace dollars_cents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dollars and cents");
+            System.Console.WriteLine(FormatMoney(5172.96));
+            Console.ReadKey();
+        }
+        public static string FormatMoney(double amount)
+        {
+            return amount.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US")).Replace(",","");
+        }
+    }
+}
