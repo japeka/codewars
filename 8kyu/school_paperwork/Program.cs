512d2a8fbedff7bee5d403f3ed838c6f6e178494
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2020-12-23 17:40:43 +0200

    two more 8kyu tasks resolved

---
+++ 8kyu/school_paperwork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/*
+Code wars
+Level: 8kyu
+Description:
+    Beginner Series #1 School Paperwork & Square
+*/
+namespace dollars_cents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("School Paperwork, Square");
+            System.Console.WriteLine(Paperwork(2,2));
+            System.Console.WriteLine(Square(2));
+            Console.ReadKey();
+        }
+
+        public static int Paperwork(int n, int m)
+        {
+            return n > 0 && m > 0 ? n * m : 0;
+        }
+
+        public static double Square(double n) => Math.Pow(n,2);
+    }
+}
