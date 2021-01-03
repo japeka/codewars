4ec1402d4a516b276c8e9c61e572547aa56a40c0
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2021-01-03 20:27:04 +0200

    factorial_decomposition kata formatting changed

--- 5kyu/factorial_decomposition/Program.cs
+++ 5kyu/factorial_decomposition/Program.cs
@@ -18,87 +18,84 @@ namespace factorial_decomposition
             Console.ReadKey();
         }
 
-    /*
-    The aim of the kata is to decompose n! (factorial n) into its prime factors.
-    Prime numbers should be in increasing order. When the exponent of a prime is 1 don't put the exponent.
-    */
-    public static string Decomp(int n)
-    {
-            IDictionary<int, int> result = new Dictionary<int, int>();
-            int index = 2;
-            for (index = 2; index <= n; index++)
-            {
-                var number = index;
-                var numberIndex = 2;
-                if (!isPrime(number))
+        /*
+        The aim of the kata is to decompose n! (factorial n) into its prime factors.
+        Prime numbers should be in increasing order. When the exponent of a prime is 1 don't put the exponent.
+        */
+        public static string Decomp(int n)
+        {
+                IDictionary<int, int> result = new Dictionary<int, int>();
+                int index = 2;
+                for (index = 2; index <= n; index++)
                 {
-                    while (!isPrime(number)) {
-                        if (number % numberIndex == 0)
-                        {
-                            if (result.ContainsKey(numberIndex))
+                    var number = index;
+                    var numberIndex = 2;
+                    if (!isPrime(number))
+                    {
+                        while (!isPrime(number)) {
+                            if (number % numberIndex == 0)
                             {
-                                result[numberIndex] += 1; 
+                                if (result.ContainsKey(numberIndex))
+                                {
+                                    result[numberIndex] += 1; 
+                                }
+                                else
+                                {
+                                    result.Add(numberIndex, 1);
+                                }
+                                number = (int)(number/numberIndex);
                             }
                             else
                             {
-                                result.Add(numberIndex, 1);
+                                numberIndex += 1; 
                             }
-                            number = (int)(number/numberIndex);
+                        }
+
+                        if (result.ContainsKey(number))
+                        {
+                            result[number] += 1;
                         }
                         else
                         {
-                            numberIndex += 1; 
+                            result.Add(number, 1);
                         }
                     }
-
-                    if (result.ContainsKey(number))
+                    if (isPrime(index))
                     {
-                         result[number] += 1;
-                    }
-                    else
-                    {
-                        result.Add(number, 1);
+                        if (result.ContainsKey(index))
+                        {
+                            result[index] += 1;
+                        }
+                        else
+                        {
+                            result.Add(index, 1);
+                        }
                     }
                 }
-                if (isPrime(index))
+                string output = "";
+                foreach (var item in result)
                 {
-                    if (result.ContainsKey(index))
+                    if (item.Key == 1) continue;
+                    if (item.Value == 1)
                     {
-                        result[index] += 1;
+                        output += item.Key + " * ";
+                        continue;
                     }
-                    else
-                    {
-                        result.Add(index, 1);
-                    }
+                    output += item.Key + "^" + item.Value + " * ";
                 }
-            }
-            string output = "";
-            foreach (var item in result)
-            {
-                if (item.Key == 1) continue;
-                if (item.Value == 1)
-                {
-                    output += item.Key + " * ";
-                    continue;
-                }
-                output += item.Key + "^" + item.Value + " * ";
-            }
-            output = output.Length > 0 ? output.Substring(0, output.Length - 3) : "";
-            return output;
-    }
-
-
-    static bool isPrime(int number)
-    {
-        var count = (int)Math.Ceiling(Math.Sqrt((double)number));
-        for (var i = 2; i <=count; i++) {
-            if (number % i == 0) {
-              return false;
-            }
+                output = output.Length > 0 ? output.Substring(0, output.Length - 3) : "";
+                return output;
         }
-        return true;
-    }
 
-
+        static bool isPrime(int number)
+        {
+            var count = (int)Math.Ceiling(Math.Sqrt((double)number));
+            for (var i = 2; i <=count; i++) {
+                if (number % i == 0) {
+                return false;
+                }
+            }
+            return true;
+        }
    }
 }
